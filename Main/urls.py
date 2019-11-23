7e97a83e91f852d3c33acf3a6f5f3938f19139cd
Alex-in-bloom <57555047+Alex-in-bloom@users.noreply.github.com>
2019-11-23 15:23:57 +0300

    last

--- Main/urls.py
+++ Main/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url('', views.home, name='Main-main'),
-    url('about/', views.about, name='Main-about'),
+    url('^$', views.home, name='Main-main'),
+    url('^about/$', views.about, name='Main-about'),
 ]
